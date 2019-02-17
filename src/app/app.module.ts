99bfb310128dab2c1ba125200343472571c8f6b8
Judionit <jmanambintsoa@gmail.com>
2019-02-17 10:48:16 +0300

    integration template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { Router, Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { FooterComponent } from './Component/footer/footer.component';
 
+const routesConfig: Routes = [
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routesConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
